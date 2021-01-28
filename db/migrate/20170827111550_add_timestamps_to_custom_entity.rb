8ab767cc33aa1c64c261bb6054f0d79cc1953123
gm <markus.gerhold@coilinnovation.at>
2021-01-28 14:54:12 +0100

    #3 allow null for fields without default value

--- db/migrate/20170827111550_add_timestamps_to_custom_entity.rb
+++ db/migrate/20170827111550_add_timestamps_to_custom_entity.rb
@@ -1,6 +1,6 @@
 class AddTimestampsToCustomEntity < ActiveRecord::Migration[4.2]
   def change
-    add_column :custom_entities, :created_at, :datetime, null: false
-    add_column :custom_entities, :updated_at, :datetime, null: false
+    add_column :custom_entities, :created_at, :datetime, null: true
+    add_column :custom_entities, :updated_at, :datetime, null: true
   end
 end
